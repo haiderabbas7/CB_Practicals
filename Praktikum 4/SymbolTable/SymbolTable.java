0cd3a0670bed9e089a1f0722417b34867cecbdd1
HaiderAbbasFH <147977345+HaiderAbbasFH@users.noreply.github.com>
2024-11-11 16:32:34 +0100

    P4 zwischenstand

---
+++ Praktikum 4/SymbolTable/SymbolTable.java
@@ -0,0 +1,31 @@
+package SymbolTable;
+
+import java.util.Hashtable;
+
+public class SymbolTable{
+    private Hashtable<String, String> symbolTabelle = new Hashtable<String, String>();
+
+    public void addConstant(String ident, String value) throws SymbolAlreadyDefinedException {
+        if (symbolTabelle.containsKey(ident)) {
+            throw new SymbolAlreadyDefinedException("Konstante " + ident + " wurde schon definiert.");
+        } else {
+            symbolTabelle.put(ident, value);
+        }
+    }
+
+    public void addVariable(String ident, String value) throws SymbolAlreadyDefinedException{
+        if(symbolTabelle.containsKey(ident)){
+            throw new SymbolAlreadyDefinedException("Variable " + ident + " wurde schon definiert.");
+        } else {
+            symbolTabelle.put(ident, value);
+        }
+    }
+
+    public String getSymbol(String ident) throws UnknownSymbolException {
+        if(!symbolTabelle.containsKey(ident)) {
+            throw new UnknownSymbolException("Symbol " + ident + " wurde nicht definiert.");
+        } else {
+            return symbolTabelle.get(ident);
+        }
+    }
+}
---
+++ Praktikum 4/SymbolTable/UnknownSymbolException.java
@@ -0,0 +1,8 @@
+package SymbolTable;
+
+public class UnknownSymbolException extends Exception {
+
+	public UnknownSymbolException(String message) {
+		super(message);
+	}
+}
