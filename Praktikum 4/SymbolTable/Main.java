417496011437af640edb99c7046699c887770908
HaiderAbbasFH <haider.abbas@alumni.fh-aachen.de>
2024-11-15 00:01:58 +0100

    P4 zwischenstand

---
+++ Praktikum 4/SymbolTable/Main.java
@@ -0,0 +1,26 @@
+package SymbolTable;
+
+import SymbolTable.SymbolTable;
+import SymbolTable.SymbolAlreadyDefinedException;
+
+public class Main {
+    public static void main(String[] args) {
+        SymbolTable symbolTable = new SymbolTable();
+
+        /*try {
+            // Test adding variables
+            String index1 = symbolTable.addVariable("var1");
+            System.out.println("Index of var1: " + index1);
+
+            String index2 = symbolTable.addVariable("var2");
+            System.out.println("Index of var2: " + index2);
+
+            // Attempt to add a duplicate variable
+            symbolTable.addVariable("var1");
+
+        } catch (SymbolAlreadyDefinedException e) {
+            System.err.println(e.getMessage());
+        }*/
+        System.out.println(Integer.toHexString(Integer.parseInt("17")));
+    }
+}
--- Praktikum 4/SymbolTable/SymbolTable.java
+++ Praktikum 4/SymbolTable/SymbolTable.java
@@ -3,25 +3,33 @@ package SymbolTable;
 import java.util.Hashtable;
 
 public class SymbolTable {
-    private Hashtable<String, Symbol> symbolTabelle = new Hashtable<String, Symbol>();
+    private Hashtable<String, String> symbolTabelle = new Hashtable<String, String>();
+    private static int nextIndex = 0;
 
     public void addConstant(String ident, String value) throws SymbolAlreadyDefinedException {
         if (symbolTabelle.containsKey(ident)) {
             throw new SymbolAlreadyDefinedException("Konstante " + ident + " wurde schon definiert.");
         } else {
-            symbolTabelle.put(ident, new Symbol(Symbol.Type.CONSTANT, value));
+            symbolTabelle.put(ident, value);
         }
     }
 
-    public void addVariable(String ident, String value) throws SymbolAlreadyDefinedException {
+    //FUNKTIONIERT
+    /**
+     * fügt die Variable mit dem Speicherwert (holt sich aus der globalen Variable) in Hashtable hinzu, wenn noch nicht drin
+     * und returned den Index der Variable, damit in der P4.jj der code erzeugt werden kann*/
+    public String addVariable(String ident) throws SymbolAlreadyDefinedException {
         if (symbolTabelle.containsKey(ident)) {
             throw new SymbolAlreadyDefinedException("Variable " + ident + " wurde schon definiert.");
         } else {
-            symbolTabelle.put(ident, new Symbol(Symbol.Type.VARIABLE, value));
+            symbolTabelle.put(ident, Integer.toString(nextIndex));
+            nextIndex++;
+            //glaube das String.format funktioniert richtig, aber nicht 100% getestet
+            return String.format("%02d", nextIndex - 1);
         }
     }
 
-    public Symbol getSymbol(String ident) throws UnknownSymbolException {
+    public String getSymbol(String ident) throws UnknownSymbolException {
         if (!symbolTabelle.containsKey(ident)) {
             throw new UnknownSymbolException("Symbol " + ident + " wurde nicht definiert.");
         } else {
